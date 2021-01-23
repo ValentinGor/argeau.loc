69aa9de7995c71908021a62cfafed6ae72b482a5
ValentinGor <v.gorbatenko@mail.ru>
2021-01-24 01:59:25 +0200

    Initial commit

---
+++ wp-content/themes/argeau/header.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Argeau
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <!-- Font CSS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="top__line">
+                    <div class="top__line_logo">
+                        <a href="index.html">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/img/logo.svg" alt="img">
+                        </a>
+                    </div>
+                    <div class="top__line_menu">
+                        <a id="menu_toggle" href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="sticky__header">
+                        <div class="sticky__header_logo">
+                            <a href="#">
+                                <img src="<?php echo get_template_directory_uri();?>/assets/img/logo-icon.svg" alt="img">
+                            </a>
+                        </div>
+                        <div class="sticky__header_menu">
+                            <div class="top__line_menu">
+                                <a id="menu_toggle_sticky" href="#">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<menu id="menu">
+    <li><a href="#">What we do</a></li>
+    <li><a href="#">Why Argeau</a></li>
+    <li><a href="#">People</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">News</a></li>
+    <li><a href="#">Login</a></li>
+</menu>
---
+++ wp-content/themes/argeau/main-page.php
@@ -0,0 +1,173 @@
+<?php
+/*
+Template Name: Main Page
+Template Post Type: page
+*/
+?>
+<?php get_header(); ?>
+
+<section>
+    <div class="first" style="background: var(--color-dark); background-image: url('<?php echo get_template_directory_uri();?>/assets/img/bg-1.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="main">
+                        <div class="main__head">
+                            <h1>Complex wealth, made simple.</h1>
+                        </div>
+                        <div class="main__slag">
+                            <p>We help wealthy families manage their assets.</p>
+                        </div>
+                    </div>
+                    <div id="scroll__bot" class="arrow">
+                        <a href="#what_we_do">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="45px" height="45px">
+                                <path fill-rule="evenodd" fill="rgb(255, 255, 255)"
+                                      d="M22.702,44.754 C10.542,44.754 0.649,34.862 0.649,22.702 C0.649,10.542 10.542,0.649 22.702,0.649 C34.861,0.649 44.754,10.542 44.754,22.702 C44.754,34.862 34.861,44.754 22.702,44.754 ZM22.702,2.852 C11.756,2.852 2.852,11.757 2.852,22.702 C2.852,33.648 11.756,42.551 22.702,42.551 C33.647,42.551 42.551,33.648 42.551,22.702 C42.551,11.757 33.647,2.852 22.702,2.852 ZM23.484,33.063 C23.268,33.278 22.990,33.377 22.702,33.377 C22.414,33.377 22.135,33.269 21.919,33.054 L15.722,26.857 C15.291,26.425 15.291,25.732 15.722,25.301 C16.154,24.870 16.847,24.870 17.278,25.301 L21.596,29.618 L21.596,13.124 C21.596,12.512 22.090,12.017 22.702,12.017 C23.313,12.017 23.808,12.512 23.808,13.124 L23.808,29.628 L28.125,25.310 C28.556,24.878 29.249,24.878 29.680,25.310 C30.112,25.741 30.112,26.434 29.680,26.866 L23.484,33.063 Z"/>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section id="what_we_do">
+    <div class="second">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="second__head">
+                        <h2>What we do?</h2>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="make">
+                        <div class="make__info">
+                            <div class="make__info_numb">
+                                01
+                            </div>
+                            <div class="make__info_head">
+                                <h3>Track all of your assets in one place</h3>
+                            </div>
+                            <div class="make__info_text">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis urna
+                                    bibendum fringilla dapibus. Etiam nulla nisi, vulputate at dapibus ac, placerat
+                                    condimentum ante.</p>
+                            </div>
+                        </div>
+                        <div class="make__imag" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/img/img-1.jpg')"></div>
+                    </div>
+                    <div class="make">
+                        <div class="make__info">
+                            <div class="make__info_numb">
+                                02
+                            </div>
+                            <div class="make__info_head">
+                                <h3>Co-ordinate with your adviser team</h3>
+                            </div>
+                            <div class="make__info_text">
+                                <p>Etiam eget imperdiet quam. In hac habitasse platea dictumst. Donec sed sem augue. In
+                                    fringilla dapibus erat, ac lobortis lorem posu. </p>
+                            </div>
+                        </div>
+                        <div class="make__imag" style="background-image: url('<?php echo  get_template_directory_uri();?>/assets/img/img-2.jpg')"></div>
+                    </div>
+                    <div class="make">
+                        <div class="make__info">
+                            <div class="make__info_numb">
+                                03
+                            </div>
+                            <div class="make__info_head">
+                                <h3>Manage your investments</h3>
+                            </div>
+                            <div class="make__info_text">
+                                <p>Nulla eu leo semper quam malesuada aliquet. Donec vel nunc a metus auctor laoreet sit
+                                    amet sed dolor.</p>
+                            </div>
+                        </div>
+                        <div class="make__imag" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/img/img-3.jpg')"></div>
+                    </div>
+                    <div class="make">
+                        <div class="make__info">
+                            <div class="make__info_numb">
+                                04
+                            </div>
+                            <div class="make__info_head">
+                                <h3>Identify new opportunities</h3>
+                            </div>
+                            <div class="make__info_text">
+                                <p>Vivamus vitae pulvinar nulla. Duis pretium mi eu finibus pretium. Nulla vitae
+                                    ultricies sapien. Suspendisse justo dolor, eleifend eget pretium sed, fermentum sed
+                                    dui. </p>
+                            </div>
+                        </div>
+                        <div class="make__imag" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/img/img-4.jpg')"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="third" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/img/bg-2.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="third__head">
+                        <h2>Why Argeau?</h2>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="wherefore">
+                        <div class="wherefore__info">
+                            <div class="wherefore__info_icon">
+                                <img src="<?php echo get_template_directory_uri();?>/assets/img/ico-1.svg" alt="img">
+                            </div>
+                            <div class="wherefore__info_head">
+                                <h4>Independence</h4>
+                            </div>
+                            <div class="wherefore__info_text">
+                                <p>Fixed fee, wholly aligned </p>
+                            </div>
+                        </div>
+
+                        <div class="wherefore__info">
+                            <div class="wherefore__info_icon">
+                                <img src="<?php echo get_template_directory_uri();?>/assets/img/ico-2.svg" alt="img">
+                            </div>
+                            <div class="wherefore__info_head">
+                                <h4>Expertise</h4>
+                            </div>
+                            <div class="wherefore__info_text">
+                                <p>Deep industry knowledge</p>
+                            </div>
+                        </div>
+
+                        <div class="wherefore__info">
+                            <div class="wherefore__info_icon">
+                                <img src="<?php echo get_template_directory_uri();?>/assets/img/ico-3.svg" alt="img">
+                            </div>
+                            <div class="wherefore__info_head">
+                                <h4>Technology</h4>
+                            </div>
+                            <div class="wherefore__info_text">
+                                <p>Tailored platform</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="start__link">
+                        <a href="#">Start today</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
